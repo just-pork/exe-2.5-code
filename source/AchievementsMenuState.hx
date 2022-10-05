b43c76df41a71d1a0926d0da9d422fa2c4897ccc
just-pork <89614860+just-pork@users.noreply.github.com>
2022-10-05 15:18:58 -0500

    Please Work.

--- source/AchievementsMenuState.hx
+++ source/AchievementsMenuState.hx
@@ -1,8 +1,5 @@
 package;
 
-#if desktop
-import Discord.DiscordClient;
-#end
 import flash.text.TextField;
 import flixel.FlxG;
 import flixel.FlxSprite;
--- source/Intro.hx
+++ source/Intro.hx
@@ -3,7 +3,6 @@ package;
 import flixel.graphics.FlxGraphic;
 import sys.FileSystem;
 #if desktop
-import Discord.DiscordClient;
 import sys.thread.Thread;
 #end
 import flixel.FlxG;
